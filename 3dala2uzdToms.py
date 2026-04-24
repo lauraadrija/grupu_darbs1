4df86eaeba0a47686f50bc6d6636e2564316734c
vpktkirsteins1-prog <vp.ktkirsteins1@kuldigasnovads.edu.lv>
2026-04-24 06:18:40 +0000

    skolotaju klase

---
+++ 3dala2uzdToms.py
@@ -0,0 +1,7 @@
+#2. uzdevums
+
+class Skolotajs:
+    def __init__(self, uzvards, stundu_skaits, tips):
+        self.uzvards = uzvards
+        self.stundu_skaits = stundu_skaits
+        self.tips = tips
