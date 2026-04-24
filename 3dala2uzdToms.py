02782f3beffcc7ff7d133324c38cbb759f7242a5
vpktkirsteins1-prog <vp.ktkirsteins1@kuldigasnovads.edu.lv>
2026-04-24 07:20:12 +0000

    izveidoju class vidusskolotajas

--- 3dala2uzdToms.py
+++ 3dala2uzdToms.py
@@ -6,10 +6,12 @@ class Skolotajs:
         self.stundu_skaits = stundu_skaits
         self.tips = tips
 
-class Sakumskolotjas:
+class SakumskolasSkolotajas:
     def __init__(self, uzvards, klase, stundu_skaits):
         super().__init__(uzvards, stundu_skaits, 1)
         self.klase = klase
     
     def izdrukat(self):
         print(f"Sākumskolas (tips - {self.tips}) skolotājs {self.uzvards} māca " f"{self.stundu_skaits} stundas {self.klase} klasē. ")
+
+class VidusskolasSkolotajas (Skolotajs):
